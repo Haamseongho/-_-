a387a71772138d74b0ca5a5ff34584a9d59cb07e
T005895 <haams704@gmail.com>
2024-08-07 14:28:47 +0900

    feat: Constraint 추가하고 잘못 셋팅한거 수정

--- Chapter01/Chapter01/ViewController.swift
+++ Chapter01/Chapter01/ViewController.swift
@@ -23,13 +23,12 @@ class ViewController: UIViewController {
     }
     // IBAction = Design -> Code
     @IBAction func btnRollPressed(_ sender: Any) {
-        diceImageViewOne.image = [UIImage(imageLiteralResourceName: "DiceOne"): UIImage(imageLiteralResourceName: "DiceTwo"),
-                                  UIImage(imageLiteralResourceName: "DiceThree"): UIImage(imageLiteralResourceName: "DiceFour"),
-                                  UIImage(imageLiteralResourceName: "DiceFive"): UIImage(imageLiteralResourceName: "DiceSix")][Int.random(in: 0...5)] // 왼쪽 주사위
+        let diceArray = [UIImage(imageLiteralResourceName: "DiceOne"), UIImage(imageLiteralResourceName: "DiceTwo")
+        ,UIImage(imageLiteralResourceName: "DiceThree"), UIImage(imageLiteralResourceName: "DiceFour"),
+                         UIImage(imageLiteralResourceName: "DiceFive"), UIImage(imageLiteralResourceName: "DiceSix")]
+        diceImageViewOne.image = diceArray[Int.random(in: 0...5)] // 왼쪽 주사위
         
-        diceImageViewTwo.image = [UIImage(imageLiteralResourceName: "DiceOne"), UIImage(imageLiteralResourceName: "DiceTwo"),
-         UIImage(imageLiteralResourceName: "DiceThree"), UIImage(imageLiteralResourceName: "DiceFour"),
-                                  UIImage(imageLiteralResourceName: "DiceFive"), UIImage(imageLiteralResourceName: "DiceSix")].randomElement() // 오른쪽 주사위
+        diceImageViewTwo.image = diceArray.randomElement() // 오른쪽 주사위
       
     }
     
